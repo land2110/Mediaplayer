9f3a96637baf0f5b1fbbf14800dc1a6e05b306ef
ducnd <land2110e@gmail.com>
2022-06-01 22:32:29 +0700

    add service and broadcast internal

--- app/src/main/java/com/t3h/demomediaplayer/activity/CalculationActivity.kt
+++ app/src/main/java/com/t3h/demomediaplayer/activity/CalculationActivity.kt
@@ -1,8 +1,10 @@
 package com.t3h.demomediaplayer.activity
 
-import android.content.Intent
+import android.content.*
 import android.os.Bundle
+import android.os.IBinder
 import android.view.View
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.t3h.demomediaplayer.R
@@ -11,24 +13,48 @@ import com.t3h.demomediaplayer.service.CalculationService
 
 class CalculationActivity : AppCompatActivity(), View.OnClickListener {
     private lateinit var binding: ActivityDemoServiceBinding
+    private var service: CalculationService?=null
+    private var conn:ServiceConnection?=null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_demo_service)
         binding.btnSum.setOnClickListener(this)
         binding.btnSub.setOnClickListener(this)
+        //dung cho service Unbound
+        registerBroadcast()
+
+        //service Bound
+        startConnectByConnectionService()
+    }
+
+    private fun registerBroadcast(){
+        val br = CalculatorBroadcast(binding.tvResult)
+        //Intent filter de dang ky
+        val filter = IntentFilter()
+        //dang ky action cua broadcast
+        filter.addAction("RESULT_CALCULATION")
+        registerReceiver(br, filter)
     }
 
     override fun onClick(view: View) {
         when (view.id) {
             R.id.btn_sum -> {
-                sum(
+//                sum(
+//                    binding.tvOne.text.toString().toInt(),
+//                    binding.tvSecond.text.toString().toInt()
+//                );
+                sumBound(
                     binding.tvOne.text.toString().toInt(),
                     binding.tvSecond.text.toString().toInt()
                 );
             }
             R.id.btn_sub -> {
-                sub(
+//                sub(
+//                    binding.tvOne.text.toString().toInt(),
+//                    binding.tvSecond.text.toString().toInt()
+//                );
+                subBound(
                     binding.tvOne.text.toString().toInt(),
                     binding.tvSecond.text.toString().toInt()
                 );
@@ -40,13 +66,72 @@ class CalculationActivity : AppCompatActivity(), View.OnClickListener {
         val intentService = Intent()
 //        intentService.setClassName("com.t3h.demomediaplayer", "com.t3h.demomediaplayer.service.CalculationService")
         intentService.setClass(this, CalculationService::class.java)
+        intentService.setAction("SUM")
+        intentService.putExtra("ONE", numberOne)
+        intentService.putExtra("TWO", numberTwo)
+        startService(intentService)
     }
 
     private fun sub(numberOne: Int, numberTwo: Int) {
+        val intentService = Intent()
+        intentService.setClass(this, CalculationService::class.java)
+        intentService.setAction("SUB")
+        intentService.putExtra("ONE", numberOne)
+        intentService.putExtra("TWO", numberTwo)
+        startService(intentService)
+    }
+    private fun sumBound(numberOne: Int, numberTwo: Int) {
+        if (service != null){
+            val result = service!!.sum(numberOne, numberTwo);
+            binding.tvResult.setText(result.toString());
+        }
+    }
 
+    private fun subBound(numberOne: Int, numberTwo: Int) {
+        if (service != null){
+            val result = service!!.sub(numberOne, numberTwo);
+            binding.tvResult.setText(result.toString());
+        }
     }
 
     private fun updateResult(result: Int) {
         binding.tvResult.setText(result.toString())
     }
+
+    //dinh nghia
+    internal class CalculatorBroadcast(val tv: TextView) : BroadcastReceiver(){
+        //noi nhan du lieu
+        override fun onReceive(context: Context, intent: Intent) {
+            val result = intent.getIntExtra("RESULT", 0)
+            tv.setText(result.toString())
+        }
+    }
+
+    private fun startConnectByConnectionService(){
+        conn = object:ServiceConnection{
+            override fun onServiceConnected(cop: ComponentName, binber: IBinder) {
+//                Lay ra servcie trong IBinder
+                service = (binber as CalculationService.MyBinder).service;
+            }
+
+            override fun onServiceDisconnected(p0: ComponentName?) {
+                service = null
+            }
+        }
+
+        val intent = Intent()
+        intent.setClass(this, CalculationService::class.java)
+        bindService(intent, conn!!, BIND_AUTO_CREATE)
+
+    }
+
+    override fun onDestroy() {
+        if (conn != null){
+            unbindService(conn!!)
+        }
+
+        super.onDestroy()
+    }
+
+
 }
--- app/src/main/java/com/t3h/demomediaplayer/service/CalculationService.kt
+++ app/src/main/java/com/t3h/demomediaplayer/service/CalculationService.kt
@@ -2,15 +2,22 @@ package com.t3h.demomediaplayer.service
 
 import android.app.Service
 import android.content.Intent
+import android.os.Binder
 import android.os.IBinder
 import android.util.Log
+
 //service la doi tuong singlton
 class CalculationService : Service() {
-    companion object{
+    companion object {
         private val TAG = CalculationService::class.java.simpleName
     }
-    override fun onBind(p0: Intent?): IBinder? {
-        return null
+
+    override fun onBind(intent: Intent): IBinder {
+        return MyBinder(this)
+    }
+
+    class MyBinder(val service: CalculationService) : Binder(){
+
     }
 
     override fun onCreate() {
@@ -18,11 +25,50 @@ class CalculationService : Service() {
         Log.d(TAG, "onCreate.....")
     }
 
+
     override fun onStartCommand(intent: Intent?, flags: Int, startId: Int): Int {
+        Log.d(TAG, "onStartCommand....")
         //loai 1: kill ung dung thi service se chet = START_NOT_STICKY
         //loai 2: kill lan 1, service song la, kill thi service chet =  START_REDELIVER_INTENT
         //loai 3: kill song lai, kill nua, chet di song lai = START_STICKY
+        if (intent != null) {
+            calculate(intent)
+        }
+
         return START_STICKY;
+    }
+
+    private fun calculate(intent: Intent) {
+        when (intent.action) {
+            "SUM" -> {
+                val one = intent.getIntExtra("ONE", 0)
+                val two = intent.getIntExtra("TWO", 0)
+                val result = one+two
+                val intentBroadcast = Intent()
+                intentBroadcast.setAction("RESULT_CALCULATION")
+                intentBroadcast.putExtra("RESULT", result)
+                //gui du lieu di
+                sendBroadcast(intentBroadcast)
+            }
+            "SUB" -> {
+                val one = intent.getIntExtra("ONE", 0)
+                val two = intent.getIntExtra("TWO", 0)
+                val result = one-two
+
+                val intentBroadcast = Intent()
+                intentBroadcast.setAction("RESULT_CALCULATION")
+                intentBroadcast.putExtra("RESULT", result)
+                //gui du lieu di
+                sendBroadcast(intentBroadcast)
+            }
+        }
+    }
+
+    fun sum(one:Int, two: Int) : Int{
+        return one+two
+    }
+    fun sub(one:Int, two: Int) : Int{
+        return one-two
     }
 
 }
